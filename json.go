e236bb79adee8adb9a9992ed76e3b267299f6b6c
Zaul_77 <zaul74945@gmail.com>
2023-08-15 18:40:20 -0500

    fix sec errors

--- json.go
+++ json.go
@@ -28,4 +28,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	}
 	w.WriteHeader(code)
 	w.Write(dat)
+	_, err = w.Write(dat)
+	if err != nil {
+		log.Printf("Critical error writing response: %s", err)
+	}
 }
